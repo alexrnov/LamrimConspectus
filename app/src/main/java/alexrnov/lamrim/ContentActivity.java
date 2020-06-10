4ae52d163e859ed332d22d53af353772f4857a3f
alexrnov <a.novopashin5@gmail.com>
2020-06-10 13:45:15 +0900

    create fragment

---
+++ app/src/main/java/alexrnov/lamrim/ContentActivity.java
@@ -0,0 +1,15 @@
+package alexrnov.lamrim;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ContentActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+
+
+}
