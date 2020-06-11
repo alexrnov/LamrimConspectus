2ebfad9913ee5756afbd1149ed26c33c6cabb23a
alexrnov <a.novopashin5@gmail.com>
2020-06-11 10:30:18 +0900

    create fragment

--- app/src/main/java/alexrnov/lamrim/ContentActivity.java
+++ app/src/main/java/alexrnov/lamrim/ContentActivity.java
@@ -3,6 +3,8 @@ package alexrnov.lamrim;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class ContentActivity extends AppCompatActivity {
 
@@ -11,14 +13,18 @@ public class ContentActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_detail);
 
+    // savedInstanceState is non-null when there is fragment state saved from
+    // previous configurations of this activity (e.g. when rotating the screen from
+    // portrait to landscape). In this case, the fragment will automatically be
+    // re-added to its container so we don't need to manually add it.
     if (savedInstanceState == null) {
-      ContentFragment fragment = new ContentFragment();
-
-      getSupportFragmentManager().beginTransaction()
-              .add(R.id.fragment_container, fragment)
-              .commit();
-
-
+      // to manage the fragments in activity, need to use FragmentManager.
+      FragmentManager manager = this.getSupportFragmentManager();
+      // to make fragment transactions in activity (such as add, remove, or replace a fragment)
+      FragmentTransaction transaction = manager.beginTransaction();
+      // add a fragment, specifying the fragment to add and the view in which to insert it.
+      transaction.add(R.id.fragment_container, new ContentFragment());
+      transaction.commit(); // call commit() for the changes to take effect.
     }
   }
 
