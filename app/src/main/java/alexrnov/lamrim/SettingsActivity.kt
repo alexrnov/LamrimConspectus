ecdec045ea42f67aa63c55c5db3ed9679028c248
alexrnov <a.novopashin5@gmail.com>
2020-06-21 21:00:37 +0900

    create settings

--- app/src/main/java/alexrnov/lamrim/SettingsActivity.kt
+++ app/src/main/java/alexrnov/lamrim/SettingsActivity.kt
@@ -1,5 +1,6 @@
 package alexrnov.lamrim
 
+import android.os.Build
 import android.os.Bundle
 import android.text.Html
 import androidx.appcompat.app.AppCompatActivity
@@ -12,29 +13,27 @@ class SettingsActivity: AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.settings_activity)
 
-
-    //Toolbar collapsingToolbarLayout = findViewById(R.id.toolbar);
-
-    //collapsingToolbarLayout.setTitle("Конспект ламрима");
     val toolbar = findViewById<Toolbar>(R.id.main_toolbar)
     setSupportActionBar(toolbar)
 
+    supportActionBar?.let {
 
-    val actionBar = supportActionBar
-    if (actionBar != null) {
-      actionBar.setDisplayHomeAsUpEnabled(true) // enable the Up button
-      actionBar.setIcon(R.drawable.home_icon)
-      actionBar.title = Html.fromHtml("<font color='#fffbbe'>" +
-              this.getString(R.string.app_name) + "</font>")
+      it.setDisplayHomeAsUpEnabled(true) // enable the Up button
+      it.setIcon(R.drawable.home_icon)
+
+      val s = "<font color='#fffbbe'>" + this.getString(R.string.app_name) + "</font>"
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        it.title = Html.fromHtml(s, Html.FROM_HTML_MODE_LEGACY)
+      } else {
+        @Suppress("DEPRECATION")
+        it.title = Html.fromHtml(s)
+      }
     }
-
 
     supportFragmentManager
             .beginTransaction()
             .replace(R.id.fragment_container, SettingsFragment())
             .commit()
-
-
   }
 
 
