258b30b66e5484332708ec6ba27dc54a098b1168
alexrnov <a.novopashin5@gmail.com>
2020-06-24 16:43:44 +0900

    create settings

--- app/src/main/java/alexrnov/lamrim/settings/SettingsActivity.kt
+++ app/src/main/java/alexrnov/lamrim/settings/SettingsActivity.kt
@@ -4,6 +4,8 @@ import alexrnov.lamrim.R
 import android.os.Build
 import android.os.Bundle
 import android.text.Html
+import android.util.Log
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.preference.Preference
@@ -56,4 +58,12 @@ class SettingsActivity: AppCompatActivity(), PreferenceFragmentCompat.OnPreferen
     return true
   }
 
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    val id = item.itemId
+    if (id == android.R.id.home) {
+      onBackPressed()
+      return true
+    }
+    return super.onOptionsItemSelected(item)
+  }
 }
