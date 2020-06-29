5e51258969127efc087630857f8796836323e3cf
alexrnov <a.novopashin5@gmail.com>
2020-06-29 18:10:24 +0900

    custom alert dialog

--- app/src/main/java/alexrnov/lamrim/AboutDialogFragment.java
+++ app/src/main/java/alexrnov/lamrim/AboutDialogFragment.java
@@ -1,14 +1,10 @@
 package alexrnov.lamrim;
 
 import android.app.Dialog;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import org.jetbrains.annotations.NotNull;
-
-
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentActivity;
