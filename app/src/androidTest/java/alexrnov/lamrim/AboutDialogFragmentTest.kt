fb207d04f2a585997fb9f73a0128d701e69213af
alexrnov <a.novopashin5@gmail.com>
2020-07-12 20:03:53 +0900

    create test for about fragment

--- app/src/androidTest/java/alexrnov/lamrim/AboutDialogFragmentTest.kt
+++ app/src/androidTest/java/alexrnov/lamrim/AboutDialogFragmentTest.kt
@@ -1,21 +1,38 @@
 package alexrnov.lamrim
 
+import alexrnov.lamrim.activities.MainActivity
 import alexrnov.lamrim.fragments.AboutDialogFragment
 import androidx.fragment.app.testing.launchFragment
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
 import org.junit.runner.RunWith
 import com.google.common.truth.Truth.assertThat
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.rule.ActivityTestRule
+import org.junit.Before
+import org.junit.Rule
 
 /**
- * To help set up the conditions for performing these tests, AndroidX provides
- * a library, FragmentScenario, to create fragments and change their state.
+ * FragmentScenario also supports testing of dialogs. Even though dialogs are instances
+ * of graphical fragments, you use the launchFragment() method so that the dialog's elements
+ * are populated in the dialog itself, rather than in the activity that launches the dialog.
  */
 @RunWith(AndroidJUnit4::class)
 class AboutDialogFragmentTest {
+
+  private lateinit var buttonLabel: String
+
+  @get:Rule
+  var activityRule: ActivityTestRule<MainActivity>
+          = ActivityTestRule(MainActivity::class.java)
+
+  @Before
+  fun initString() {
+    buttonLabel = activityRule.activity.getString(R.string.ok_button)
+  }
 
   @Test
   fun showDialog() {
@@ -23,15 +40,18 @@ class AboutDialogFragmentTest {
     scenario.onFragment { fragment ->
       assertThat(fragment.dialog).isNotNull()
       assertThat(fragment.requireDialog().isShowing).isTrue()
+    }
+    onView(withId(R.id.about_app_text)).check(matches(isDisplayed()))
+    onView(withId(R.id.close_dialog_button)).check(matches(isDisplayed()))
+    onView(withId(R.id.close_dialog_button)).check(matches(withText(buttonLabel)))
+
+    scenario.onFragment { fragment ->
       fragment.dismiss()
       // is deprecated
       //fragment.requireFragmentManager().executePendingTransactions()
       fragment.parentFragmentManager.executePendingTransactions()
       assertThat(fragment.dialog).isNull()
     }
-
-    // Assumes that the dialog had a button
-    // containing the text "Cancel".
-    // onView(withText("Cancel")).check(doesNotExist())
+    onView(withText(buttonLabel)).check(doesNotExist())
   }
 }
