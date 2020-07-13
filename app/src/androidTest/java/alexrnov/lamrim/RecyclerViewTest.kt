74957112829955bf068daf1ced25587d26f07d13
alexrnov <a.novopashin5@gmail.com>
2020-07-13 20:00:57 +0900

    create recyclerView test

--- app/src/androidTest/java/alexrnov/lamrim/RecyclerViewTest.kt
+++ app/src/androidTest/java/alexrnov/lamrim/RecyclerViewTest.kt
@@ -1,13 +1,16 @@
 package alexrnov.lamrim
 
+import alexrnov.lamrim.TestUtils.load
 import alexrnov.lamrim.activities.MainActivity
 import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.RootMatchers
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
@@ -29,8 +32,12 @@ class RecyclerViewTest {
   fun clickFirstItem() {
     onView(withId(R.id.main_menu))
             .inRoot(RootMatchers.withDecorView(Matchers.`is`(activityRule.activity.window.decorView)))
-            .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(1, click()))
 
+    val input = activityRule.activity.resources.openRawResource(R.raw.preview_text0)
+    val s = load(input)
+
+    onView(withId(R.id.preview_text)).check(matches(isDisplayed()))
   }
 
   @Test
@@ -47,6 +54,5 @@ class RecyclerViewTest {
     onView(withId(R.id.main_menu))
             .inRoot(RootMatchers.withDecorView(Matchers.`is`(activityRule.activity.window.decorView)))
             .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(lastItem, click()))
-
   }
 }
