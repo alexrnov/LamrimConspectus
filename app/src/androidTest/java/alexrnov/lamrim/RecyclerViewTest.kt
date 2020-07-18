6eeefec4a755fba77f7d9e841ef70a5323720978
alexrnov <a.novopashin5@gmail.com>
2020-07-18 12:41:48 +0900

    add to recycler test

--- app/src/androidTest/java/alexrnov/lamrim/RecyclerViewTest.kt
+++ app/src/androidTest/java/alexrnov/lamrim/RecyclerViewTest.kt
@@ -6,7 +6,6 @@ import android.content.pm.ActivityInfo
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
@@ -44,16 +43,23 @@ class RecyclerViewTest {
     onView(withId(R.id.main_menu))
             .inRoot(RootMatchers.withDecorView(Matchers.`is`(activityRule.activity.window.decorView)))
             .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(1, click()))
+
     onView(withId(R.id.preview_text)).check(matches(isDisplayed()))
     onView(withId(R.id.preview_text)).check(matches(TextMatches.isLength(previewText.length)))
+    onView(withId(R.id.details_button)).check(matches(isDisplayed()))
+    onView(withId(R.id.details_button)).check(matches(withText(R.string.details_button_text)))
+
 
     input = activityRule.activity.resources.openRawResource(R.raw.preview_text2)
     previewText = loadTextFromFile(input)
     onView(withId(R.id.main_menu))
             .inRoot(RootMatchers.withDecorView(Matchers.`is`(activityRule.activity.window.decorView)))
             .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(2, click()))
+
     onView(withId(R.id.preview_text)).check(matches(isDisplayed()))
     onView(withId(R.id.preview_text)).check(matches(TextMatches.isLength(previewText.length)))
+    onView(withId(R.id.details_button)).check(matches(isDisplayed()))
+    onView(withId(R.id.details_button)).check(matches(withText(R.string.details_button_text)))
   }
 
   @Test
@@ -90,8 +96,46 @@ class RecyclerViewTest {
                     Matchers.`is`(activityRule.activity.window.decorView)))
             .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(lastItem))
 
+    val input = activityRule.activity.resources.openRawResource(R.raw.preview_text18)
+    val previewText = loadTextFromFile(input)
+
+    // click last element
     onView(withId(R.id.main_menu))
             .inRoot(RootMatchers.withDecorView(Matchers.`is`(activityRule.activity.window.decorView)))
             .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(lastItem, click()))
+
+    onView(withId(R.id.preview_text)).check(matches(isDisplayed()))
+    onView(withId(R.id.preview_text)).check(matches(TextMatches.isLength(previewText.length)))
+    onView(withId(R.id.details_button)).check(matches(isDisplayed()))
+    onView(withId(R.id.details_button)).check(matches(withText(R.string.details_button_text)))
   }
+
+  @Test
+  fun clickEndItemPortraitOrientation() {
+    // set portrait orientation for test
+    activityRule.activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+    val recyclerView = activityRule.activity.findViewById<RecyclerView>(R.id.main_menu)
+    val itemCount = recyclerView.adapter?.itemCount
+
+    val lastItem = itemCount?.minus(1)?: 0
+    // scroll to end of position
+    onView(withId(R.id.main_menu)).inRoot(RootMatchers.withDecorView(
+                    Matchers.`is`(activityRule.activity.window.decorView)))
+            .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(lastItem))
+
+    val input = activityRule.activity.resources.openRawResource(R.raw.details_text18)
+    val detailsText = loadTextFromFile(input)
+
+    // click last element
+    onView(withId(R.id.main_menu))
+            .inRoot(RootMatchers.withDecorView(Matchers.`is`(activityRule.activity.window.decorView)))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(lastItem, click()))
+
+    onView(withText(R.id.frame_main_menu)).check(doesNotExist())
+    onView(withId(R.id.fragment_details)).check(matches(isDisplayed()))
+    onView(withId(R.id.details_text)).check(matches(isDisplayed()))
+    onView(withId(R.id.details_text)).check(matches(TextMatches.isLength(detailsText.length)))
+  }
+
 }
